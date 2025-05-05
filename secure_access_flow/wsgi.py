dd0b121b8353dc4f1b38b2f58c7f2d6707734307
mihajlovicn10 <nikola.mihajlovic1@protonmail.com>
2025-05-05 12:22:14 +0300

    Fix Python path in wsgi.py for Heroku deployment

--- secure_access_flow/wsgi.py
+++ secure_access_flow/wsgi.py
@@ -10,13 +10,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 import sys
 
-from django.core.wsgi import get_wsgi_application
-
 # Add the project directory to the Python path
-path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if path not in sys.path:
-    sys.path.append(path)
+project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if project_path not in sys.path:
+    sys.path.insert(0, project_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'secure_access_flow.settings')
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
